543722aaad6e840d7b26fc95d99b8e171fa4d348
hechenhui <hch01910027@taobao.com>
2026-01-19 14:59:01 +0800

    fix: 优化代码

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import { getSavedTheme, applyTheme } from './utils/theme';
+
+// 在首屏渲染前应用主题，减少闪动
+applyTheme(getSavedTheme());
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
